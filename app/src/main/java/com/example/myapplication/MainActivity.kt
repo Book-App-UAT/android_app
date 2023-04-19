3966721a35567a1928f8df2b0f289340719a499e
brunofavs <bruno.favs@ua.pt>
2023-04-19 14:03:02 +0200

    Added user case diagram's pdf

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -11,6 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +39,7 @@ fun TitleLayout(){
         Text(text = "yeah", fontSize = 32.sp,
             color = Color(color = 0xFFFFC265 )
         )
+        Image(painter = painterResource(id = R.drawable.book_logo), contentDescription = "")
     }
 }
 @Composable
